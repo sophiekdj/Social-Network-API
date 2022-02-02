f98f527274e3aced49b070f37dc25819cefdcb12
sophiekdj <s.dejong057@gmail.com>
2022-02-02 23:08:43 +0800

    added /friend and /reaction routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,9 +1,3 @@
-// get all thoughts
-// get single thought by id
-// post to create new thought - push thought id to creators user id, thoughts array field
-// put - update thought by id
-// delete - delete thought by id
-
 const router = require("express").Router();
 const {
   getThoughts,
@@ -11,18 +5,32 @@ const {
   createThought,
   updateThought,
   deleteThought,
+  addReaction,
+  deleteReaction,
 } = require("../../controllers/thoughtController");
 
-// /api/users
+// get all thoughts
+// /api/thoughts
 router.route("/").get(getThoughts).post(createThought);
 
-// /api/users/:userId
-router.route("/:userId").get(getSingleThought);
+// get single thought by id
+// /api/thoughts/:thoughtId
+router.route("/:thoughtId").get(getSingleThought);
 
-// /api/users/:userId
-router.route("/:userId").put(updateThought);
+// post to create new thought - push thought id to creators user id, thoughts array field
+// /api/thoughts/:thoughtId
+router.route("/:thoughtId").put(updateThought);
 
-// /api/users/:userId
-router.route("/:userId").delete(deleteThought);
+// delete - delete thought by id
+// /api/thoughts/:thoughtId
+router.route("/:thoughtId").delete(deleteThought);
+
+// create reaction stored in single thoughts reactions array field
+// /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// delete to pull and remove reaction by reactionId value
+// /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").delete(deleteReaction);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,9 +1,3 @@
-// get all users
-// get one user by id - populated friend and thought data
-// post a new user
-// put - update a user by its id
-// delete - remove user by id - bonus: remove users associated thoughts when deleted
-
 const router = require("express").Router();
 const {
   getUsers,
@@ -11,18 +5,32 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  addFriend,
+  removeFriend,
 } = require("../../controllers/userController");
 
+// get all users +++ post a new user
 // /api/users
 router.route("/").get(getUsers).post(createUser);
 
+// get one user by id - populated friend and thought data
 // /api/users/:userId
 router.route("/:userId").get(getSingleUser);
 
+// put - update a user by its id
 // /api/users/:userId
 router.route("/:userId").put(updateUser);
 
+// delete - remove user by id - bonus: remove users associated thoughts when deleted
 // /api/users/:userId
 router.route("/:userId").delete(deleteUser);
+
+// post - add new friend to users friend list
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend);
+
+// delete - remove friend from users friend list
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").delete(removeFriend);
 
 module.exports = router;
